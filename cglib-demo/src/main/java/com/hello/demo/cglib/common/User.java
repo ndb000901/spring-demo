0cb75d53df6e0ee8ffe955ed3111ff68ade679b8
hello <hello@example.com>
2025-02-24 11:45:07 +0000

    feat(project): add cglib-demo

--- cglib-demo/src/main/java/com/hello/demo/cglib/common/User.java
+++ cglib-demo/src/main/java/com/hello/demo/cglib/common/User.java
@@ -1,4 +1,12 @@
 package com.hello.demo.cglib.common;
 
 public class User {
+
+    private String name;
+    
+    private int age;
+
+    public void sayHello() {
+        System.out.println("hello");
+    }
 }
--- cglib-demo/src/main/java/com/hello/demo/cglib/common/UserProxy.java
+++ cglib-demo/src/main/java/com/hello/demo/cglib/common/UserProxy.java
@@ -1,4 +1,31 @@
 package com.hello.demo.cglib.common;
 
-public class UserProxy {
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class UserProxy implements MethodInterceptor {
+
+    private Object target;
+
+    public UserProxy(Object target) {
+        this.target = target;
+    }
+
+    public Object getProxyInstance() {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(target.getClass());
+        enhancer.setCallback(this);
+        return enhancer.create();
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println(method.getName() + " 方法执行前");
+        Object result = methodProxy.invokeSuper(o, objects);
+        System.out.println(method.getName() + " 方法执行后");
+        return null;
+    }
 }
