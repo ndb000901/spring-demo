8b03d9833ed6b3ff533aa6a3b5486e41ac9e5dad
hello <hello@example.com>
2025-02-25 14:21:43 +0000

    feat(project): add spring-resource

--- spring-resource/src/main/java/com/hello/demo/spring/resource/ClassPathResourceDemo.java
+++ spring-resource/src/main/java/com/hello/demo/spring/resource/ClassPathResourceDemo.java
@@ -1,4 +1,25 @@
 package com.hello.demo.spring.resource;
 
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ClassPathResourceDemo {
+
+    public static void main(String[] args) throws IOException {
+
+        ClassPathResource resource = new ClassPathResource("bean.xml");
+
+        System.out.println(resource.exists());
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+
+    }
 }
