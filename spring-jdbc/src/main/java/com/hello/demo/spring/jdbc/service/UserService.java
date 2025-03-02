210d596feb34b0378c5aefae1bf80e8a65d8efb4
hello <hello@example.com>
2025-03-02 12:53:44 +0000

    feat(project): add spring-jdbc

--- spring-jdbc/src/main/java/com/hello/demo/spring/jdbc/service/UserService.java
+++ spring-jdbc/src/main/java/com/hello/demo/spring/jdbc/service/UserService.java
@@ -1,4 +1,15 @@
 package com.hello.demo.spring.jdbc.service;
 
+import com.hello.demo.spring.jdbc.common.User;
+
 public interface UserService {
+
+    public User getUser(long id);
+
+    public void addUser(User user);
+
+    public void updateUser(User user);
+
+    public void deleteUser(long id);
+
 }
--- spring-jdbc/src/main/java/com/hello/demo/spring/jdbc/service/UserServiceImpl.java
+++ spring-jdbc/src/main/java/com/hello/demo/spring/jdbc/service/UserServiceImpl.java
@@ -1,4 +1,33 @@
 package com.hello.demo.spring.jdbc.service;
 
-public class UserServiceImpl {
+import com.hello.demo.spring.jdbc.common.User;
+import com.hello.demo.spring.jdbc.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public User getUser(long id) {
+        return this.userDao.getUser(id);
+    }
+
+    @Override
+    public void addUser(User user) {
+        this.userDao.addUser(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        this.userDao.updateUser(user);
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        this.userDao.deleteUser(id);
+    }
 }
